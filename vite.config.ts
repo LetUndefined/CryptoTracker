3f55136410d7e960ecfb11b6f397274fb6dd4351
Sandro <sandroodangelo@hotmail.com>
2026-01-11 21:50:48 +0100

    fixed for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/CryptoTracker/',
   plugins: [vue()],
   resolve: {
     alias: {
